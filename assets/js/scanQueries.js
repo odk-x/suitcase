36dc79084311774259869a5dfe67fd4d7b8d25bb
Kamil Kalfas <kkalfas@soldevelo.com>
2015-05-13 12:47:57 +0200

    VR#176 VR#177: Added scanQueries.js. Now month and hsa name are displayed on result page.

---
+++ assets/js/scanQueries.js
@@ -0,0 +1,61 @@
+/* global control */
+'use strict';
+
+var scanQueries = {};
+
+scanQueries.month = 'month';
+scanQueries.hsa_name = 'hsa'; 
+
+scanQueries.getKeysToAppendToURL = function(newMonth, newHSAName) {
+    var result =
+        '?' +
+        scanQueries.month +
+        '=' +
+        encodeURIComponent(newMonth) +
+        '&' +
+        scanQueries.hsa_name +
+        '=' +
+        encodeURIComponent(newHSAName);
+    return result;
+};
+
+scanQueries.getQueryParameter = function(key) {
+    var href = document.location.search;
+    var startIndex = href.search(key);
+    if (startIndex < 0) {
+        console.log('requested query parameter not found: ' + key);
+        return null;
+    }
+    // Then we want the substring beginning after "key=".
+    var indexOfValue = startIndex + key.length + 1;  // 1 for '='
+    // And now it's possible that we have more than a single url parameter, so
+    // only take as many characters as we need. We'll stop at the first &,
+    // which is what specifies more keys.
+    var fromValueOnwards = href.substring(indexOfValue);
+    var stopAt = fromValueOnwards.search('&');
+    if (stopAt < 0) {
+        return decodeURIComponent(fromValueOnwards);
+    } else {
+        return decodeURIComponent(fromValueOnwards.substring(0, stopAt));
+    }
+};
+
+scanQueries.showQueryParams = function() {
+    var month = scanQueries.getQueryParameter(scanQueries.month);
+    var hsa_name = scanQueries.getQueryParameter(scanQueries.hsa_name);
+    var idCell = $('#month');
+    $(idCell).html(month);
+    idCell = $('#hsa');
+    $(idCell).html(hsa_name);
+};
+
+scanQueries.showResultPage = function(pagePath) {
+    $('#show-report-button').on('click', function() {
+        var month = $("#month").val();
+        var hsa_name = $("#hsa").val();
+        var queryString = scanQueries.getKeysToAppendToURL(month, hsa_name);
+        var url = control.getFileAsUrl(pagePath + queryString);
+        window.location.href = url;
+    });
+
+};
