abced84ed7c53923ab161e0e244d75abca4175bb
shubhamr837 <shubhamr837@gmail.com>
2021-06-09 12:00:58 +0530

    Change String value of ButtonAction enum

--- src/main/java/org/opendatakit/suitcase/utils/ButtonAction.java
+++ src/main/java/org/opendatakit/suitcase/utils/ButtonAction.java
@@ -1,16 +1,15 @@
 package org.opendatakit.suitcase.utils;
 
 public enum ButtonAction {
-    REMOVE("Remove"),
-    ADD("Add");
-    String action;
+    REMOVE("REMOVE"),
+    ADD("ADD");
+    final String action;
 
     ButtonAction(String s) {
         this.action = s;
     }
 
-    public String getStringValueOfAction()
-    {
+    public String getStringValueOfAction() {
         return this.action;
     }
 }
