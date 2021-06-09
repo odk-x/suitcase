2e6329b7ddbb542409074571b6d56fba3b6f292d
shubhamr837 <shubhamr837@gmail.com>
2021-06-09 12:08:52 +0530

    implement required changes

--- src/main/java/org/opendatakit/suitcase/ui/DropdownPanel.java
+++ src/main/java/org/opendatakit/suitcase/ui/DropdownPanel.java
@@ -7,21 +7,13 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class DropdownPanel extends JPanel{
-    private GridBagConstraints gbc;
-    private ActionListener addActionListener;
-    private JComboBox dropdown;
+    private static final String ADD_BUTTON_LABEL = "Add";
 
-    public DropdownPanel(String label, JComboBox dropdown, ActionListener addActionListener) {
+    public DropdownPanel(String label, JComboBox<String> dropdown, ActionListener addActionListener) {
         super(new FlowLayout(FlowLayout.LEFT));  // flow layout with left alignment of elements
 
-        this.gbc = LayoutDefault.getDefaultGbc();
-        this.gbc.gridx = 0;
-        this.gbc.gridy = GridBagConstraints.RELATIVE;
-        this.addActionListener=addActionListener;   // action listener for the add button
-        this.dropdown=dropdown;
-
         buildLabelPanel(label);
-        buildDropDown();
+        buildDropDown(dropdown,addActionListener);
 
     }
 
@@ -36,13 +28,16 @@ public class DropdownPanel extends JPanel{
     }
 
     // build the drop down of table ids with an add button
-    public void buildDropDown() {
+    private void buildDropDown(JComboBox<String> dropdown,ActionListener addActionListener) {
         JPanel inputPanel = new JPanel(new GridLayout(1,2));
 
         inputPanel.add(dropdown);
         JButton addButton = new JButton();
+        dropdown.setMaximumSize(new Dimension(200,25));
+        dropdown.setMinimumSize(new Dimension(150,25));
+        dropdown.setPreferredSize(new Dimension(200,25));
         addButton.setActionCommand(ButtonAction.ADD.getStringValueOfAction());
-        addButton.setText(ButtonAction.ADD.getStringValueOfAction());
+        addButton.setText(ADD_BUTTON_LABEL);
         addButton.addActionListener(addActionListener);
 
         inputPanel.add(addButton);
