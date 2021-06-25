996f0d55be3f4d1c46ed6c69218bf6d5d6f4dcff
shubhamr837 <shubhamr837@gmail.com>
2021-06-25 20:43:09 +0530

    Add property file name to constants

--- src/main/java/org/opendatakit/suitcase/utils/SuitcaseConst.java
+++ src/main/java/org/opendatakit/suitcase/utils/SuitcaseConst.java
@@ -2,6 +2,7 @@ package org.opendatakit.suitcase.utils;
 
 public class SuitcaseConst {
   public static final String APP_NAME = "ODK org.opendatakit.suitcase.Suitcase";
+  public static final String PROPERTIES_FILE = "suitcase.properties";
   public static final String ANONYMOUS_USER = "anonymous";
   public static final String DEFAULT_LOCALE = "default";
   public static final String MAIL_TO_PREFIX = "mailto:";
