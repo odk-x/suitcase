40acc7af8d67038f87a788e350fc39fcd9763a1d
Shubhamr837 <Shubhamr837@gmail.com>
2021-08-06 14:33:52 +0530

    Add new Color

--- src/main/java/org/opendatakit/suitcase/ui/LayoutConsts.java
+++ src/main/java/org/opendatakit/suitcase/ui/LayoutConsts.java
@@ -8,6 +8,7 @@ public class LayoutConsts {
   public static final int WINDOW_WIDTH = 900;
   public static final int WINDOW_HEIGHT = 600;
   public static final Color BUTTON_BACKGROUND_COLOR = Color.MAGENTA;
+  public static final Color WARNING_BACKGROUND_COLOR = Color.RED;
   public static final Color BUTTON_FOREGROUND_COLOR = Color.WHITE;
   public static final Border BUTTON_BORDER = BorderFactory.createLineBorder(Color.BLACK);
   public static final String ODKX_LOGO_FILE_NAME = "logo.png";
